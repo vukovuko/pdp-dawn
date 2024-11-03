5b40eb83fee165761307e78b39aa295ade502f3f
Vuko Vukasinovic <vukasinovicvuko5@gmail.com>
2024-11-04 00:21:54 +0100

    Added product information section

---
+++ assets/section-product-information.js
@@ -0,0 +1,70 @@
+if (!customElements.get('swiper-component')) {
+  class SwiperComponent extends HTMLElement {
+
+    constructor() {
+      super();
+      this.swiperWrapper = this.querySelector('.swiper-wrapper');
+      this.swiperButtonNext = this.querySelector('.swiper-button-next');
+      this.swiperButtonPrev = this.querySelector('.swiper-button-prev');
+      this.settings = {
+        slidesPerView: parseInt(this.dataset.slidesPerView) || 1,
+        freemode: this.dataset.freemode === 'true',
+        watchSlidesProgress: this.dataset.watchSlidesProgress === 'true',
+        spaceBetween: parseInt(this.dataset.spaceBetween) || 0,
+        navigation: {
+          nextEl: this.swiperButtonNext,
+          prevEl: this.swiperButtonPrev,
+        },
+        thumbs: this.dataset.thumbs ? { swiper: null } : undefined,
+      }
+    }
+
+    connectedCallback() {
+      if (!this.dataset.thumbs) {
+        this.swiper = new Swiper(this, this.settings);
+        return;
+      }
+
+      this.initializeWithThumbs();
+    }
+
+    initializeWithThumbs() {
+      const thumbsElement = document.querySelector(this.dataset.thumbs);
+
+      const initializeMainSwiper = () => {
+        if (thumbsElement && thumbsElement.swiper) {
+          this.settings.thumbs.swiper = thumbsElement.swiper;
+          this.swiper = new Swiper(this, this.settings);
+        } else {
+          requestAnimationFrame(initializeMainSwiper);
+        }
+      };
+
+      initializeMainSwiper();
+    }
+  }
+
+  customElements.define('swiper-component', SwiperComponent);
+}
+
+if (!customElements.get('product-accordion')) {
+  class ProductAccordion extends HTMLElement {
+    constructor() {
+      super();
+      this.headers = this.querySelectorAll('.accordion-header');
+
+      this.headers.forEach(header => {
+        header.addEventListener('click', () => this.toggleAccordion(header));
+      });
+    }
+
+    toggleAccordion(header) {
+      const content = header.nextElementSibling;
+
+      header.classList.toggle('accordion__item--active');
+      content.classList.toggle('accordion-content--open');
+    }
+  }
+
+  customElements.define('product-accordion', ProductAccordion);
+}
